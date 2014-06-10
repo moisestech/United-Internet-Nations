82bdd9dbebc1846e9dee35b40fe29f5d197a765e
Moises Sanabria <sesmoi.data@gmail.com>
2014-06-10 19:53:57 -0400

    continued working on project and scene files

---
+++ Converters/obj/kim.js
@@ -0,0 +1,23 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "kim.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 13418,
+        "faces"         : 13258,
+        "normals"       : 27025,
+        "uvs"           : 16985,
+        "materials"     : 1
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "initialShadingGroup"
+	}],
+
+    "buffers": "kim.bin"
+
+}
